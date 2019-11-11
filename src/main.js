60e8dbff9ce4e88c0ab04c633604c14f83a5532f
WangJing <subaoing@163.com>
2019-11-11 11:02:46 +0800

    初始化配置

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import animated from 'animate.css'
+import './plugins/element.js'
+
+Vue.use(animated)
+
 Vue.config.productionTip = false
 
 new Vue({
